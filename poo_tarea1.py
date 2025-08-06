d6a56428660981228df23abff962dd64c4102c40
Manu ZZ <manu.brambilla94@gmail.com>
2025-08-06 17:10:49 -0300

    Class Auto y Moto Heredado de Vehículo

--- poo_tarea1.py
+++ poo_tarea1.py
@@ -1,25 +1,71 @@
-class Personaje:
-    def __init__(self, nombre, def_fis, def_mag, fuerza, inteligencia, vida, arma):
-        self.nombre = nombre
-        self.fuerza = fuerza
-        self.inteligencia = inteligencia
-        self.def_fis = def_fis
-        self.def_mag = def_mag
-        self.vida = vida
-        self.arma = arma
-#ATRIBUTOS PERSONAJE
-    def atributos(self):
-        print(self.nombre, ":", sep="")
-        print("-Fuerza:", self.fuerza)
-        print("-Inteligencia:", self.inteligencia)
-        print("-Defensa Física:", self.def_fis)
-        print("-Defensa Mágica:", self.def_mag)
-        print("-Vida:", self.vida)
-        print("-Arma:", self.arma)
-#ACCIONES
-    def subir_nivel(self):
-        pass
+class Vehiculo:
+    def __init__(self, marca, modelo, anio, titular, patente, vendido):
+        self.marca = marca
+        self.modelo = modelo
+        self.anio = anio
+        self.__titular = titular
+        self.__patente = patente
+        self.vendido = vendido
+
+# Métodos para mover vehículo
+    def __encender(self):
+        print(f"{self.marca} {self.modelo} está encendido y listo para moverse...")
+
+    def __apagar(self):
+        print(f"Se apagó {self.marca} {self.modelo}")
+
+    def __andar(self):
+        print(f"{self.marca} {self.modelo} está andando")
+
+#Mostrar especificaciones 
+    def especificaciones(self):
+        #print(f"El vehículo seleccionado es:")
+        print(f"Marca: {self.marca}")
+        print(f"Modelo: {self.modelo}")
+        print(f"Año: {self.anio}")
+        print(f"Titular: {self.__titular}")
+        print(f"Patente: {self.__patente}")
+        print(f"Vendido: {self.vendido}")
+
+    def se_vende(self):
+#Modificar titular, GET para extraer el dato
+        def get_titular(self):
+            return self.__titular
+#En caso de self.vendido cambie a TRUE, el titular va a cambiar, si no seguirá con el mismo dato declarado con el objeto. 
+        def set_titular(self, titular):
+            if self.vendido == False:
+                print(f"El vehículo todavía pertenece a: {self.__titular}")
+            else:
+                self.__titular = titular
+                print(f"Ahora el vehículo pertenece a: {self.__titular}")
 
 
-manu = Personaje()
-print(manu)
+#Heredamos "Vehículo" y lo convertimos en AUTO
+class Auto(Vehiculo):
+    def __init__(self, marca, modelo, anio, titular, patente, vendido, puertas):
+        super().__init__(marca, modelo, anio, titular, patente, vendido)
+        self.puertas = puertas
+    #Agregamos cantidad de puertas como nuevo atributo para autos
+    def especificaciones(self):
+        print("El AUTO es:")
+        super().especificaciones()
+        print(f"Cantidad de puertas: {self.puertas}")
+        print("-------------------------------------------------------")
+
+#Heredamos "Vehículo" y lo convertimos en MOTO
+class Moto(Vehiculo):
+    def __init__(self, marca, modelo, anio, titular, patente, vendido, cilindrada):
+        super().__init__(marca, modelo, anio, titular, patente, vendido)
+        self.cilindrada = cilindrada
+    #Agregamos como nuevo dato la cilindrada de la moto
+    def especificaciones(self):
+        print("La MOTO es:")
+        super().especificaciones()
+        print(f"Cilindrada: {self.cilindrada}")
+        print("-------------------------------------------------------")
+
+mi_auto = Auto("Fiat", "147 Vivace", 1994, "Manuel Brambilla", "RNA 123", False, "3")
+mi_auto.especificaciones()
+
+mi_moto = Moto("Honda", "Wave", 2020, "Manuel Brambilla", "AA RNA 123", False, 125)
+mi_moto.especificaciones()
